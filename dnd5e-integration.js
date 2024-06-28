f3a3ae40d5bf3c1baf069dc18ce2e02401c9a464
Travis <9389564+tn-wright@users.noreply.github.com>
2024-06-28 13:16:58 -0500

    Add user detection to hooks

--- dnd5e-integration.js
+++ dnd5e-integration.js
@@ -1,9 +1,22 @@
 Hooks.on("dnd5e.restCompleted", (actor, updates, rest) => {
+  const kairosOwnerId = "fSrPDDU2iLLuhZ3I"
+
+  // If the owner of kairos is online, only run this for them
+  if (game.users.get(kairosOwnerId).active && game.user.id !== kairosOwnerId) {
+    return;
+  }
+
+  // If the owner of kairos is not online, let this be run for the GM
+  if (!game.users.get(kairosOwnerId).active && !game.user.isGM) {
+    return;
+  }
+
   // Only do something if Kairos is taking a long rest
   if(actor.name !== "Kairos" || !updates.longRest) {
     return;
   }
 
-  actor.getEmbeddedCollection("items").find((i) => i.name === "Trance").use();
+  // Use the Trance feature
+  actor.items.find((i) => i.name === "Trance").use();
 
 });
