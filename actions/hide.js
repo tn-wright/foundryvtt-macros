4f8e68f3defb125d959f09e9edf9458913e91e3d
tn-wright <9389564+tn-wright@users.noreply.github.com>
2024-07-01 16:06:28 -0500

    Fix hide action with flags

--- actions/hide.js
+++ actions/hide.js
@@ -1,9 +1,13 @@
 const userActor = item.actor;
-const hidingEffect =  userActor.effects.find((e) => e.name === "Hiding");
 
-if(hidingEffect) {
+wasHidingFlag = userActor.getFlag("world", "was-hiding");
+
+if(wasHidingFlag) {
+  const hidingEffect =  userActor.effects.find((e) => e.name === "Hiding");
   await userActor.deleteEmbeddedDocuments("ActiveEffect", [hidingEffect.id]);
+  userActor.unsetFlag("world", "was-hiding");
   return;
 } else {
   await userActor.rollSkill("ste");
+  userActor.setFlag("world", "was-hiding", true);
 }
