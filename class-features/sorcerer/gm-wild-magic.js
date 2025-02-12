52d574a4d7618baabd9f84ff3b153db714a90f55
Travis <9389564+tn-wright@users.noreply.github.com>
2025-02-11 23:39:02 -0600

    Automatically roll wild magic for Tides of Chaos

--- class-features/sorcerer/gm-wild-magic.js
+++ class-features/sorcerer/gm-wild-magic.js
@@ -1,7 +1,5 @@
 const characterName = scope.charName;
 const spellCast = scope.spellCast;
-const tidesAvailable = scope.tidesAvailable;
-const tidesMax = scope.tidesMax;
 
 let returnValue = null;
 
@@ -15,26 +13,16 @@ let dialogButtons = {
   },
 };
 
-if (tidesAvailable < tidesMax) {
-  dialogButtons["tides"] = {
-    icon: "<i class='fa-solid fa-hand-sparkles'></i>",
-    label: "Tides of Chaos",
-    callback: () => {
-      returnValue = "tides";
-    },
-  };
-}
-
 dialogButtons["none"] = {
   icon: "<i class='fas fa-times'></i>",
   label: "No Surge",
   callback: () => {},
 };
+
 await Dialog.wait({
   title: "Wild Magic Surge",
   content: `<p><span style="font-weight:bold">Character</span>: ${characterName}</p>
-  <p><span style="font-weight:bold">Spell</span>: ${spellCast}</p>
-  <p><span style="font-weight:bold">Tides of Chaos</span>: ${tidesAvailable}/${tidesMax}</p>`,
+  <p><span style="font-weight:bold">Spell</span>: ${spellCast}</p>`,
   buttons: dialogButtons,
   default: "roll",
 });
